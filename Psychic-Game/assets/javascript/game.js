7c9f46fc3e6946f8f056e9788c5ea7162a7adff0
Dkirchenberg <dekirchenberg@gmail.com>
2018-02-22 16:39:49 -0500

    Additionaal Updates

--- Psychic-Game/assets/javascript/game.js
+++ Psychic-Game/assets/javascript/game.js
@@ -1,1 +1,18 @@
-<script src="assets/javasript"></script>
+<script src="assets/javasript">
+
+
+var choices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+wins = 0;
+losses = 0;
+guessesSoFar = []; 
+guessesLeft = 9;
+
+
+var computerCurrentChoice = Choices[Math.floor(Math.random() * 
+    lettersIndex.length)];
+
+
+
+
+</script>
