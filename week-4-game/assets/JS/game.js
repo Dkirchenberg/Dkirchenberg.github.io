e089700f2a0f6bc290a36c097677f0899e23c16e
Dkirchenberg <dekirchenberg@gmail.com>
2018-02-28 22:02:58 -0500

    updated homework

---
+++ week-4-game/assets/JS/game.js
@@ -0,0 +1,52 @@
+<script style= "text/javascript">
+
+  
+
+var first = document.getElementById("purple-crystal");
+
+var second = document.getElementById("green-crystal");
+
+var third = document.getElementById("blue-crystal");
+
+var fourth = document.getElementById("white-crystal");
+
+$("body").on("click", "#purple-crystal", function() {
+
+ 
+  alert("You clicked a crystal!")
+
+});
+
+$("body").on("click", "#purple-crystal", function() {
+
+ 
+alert("You clicked a crystal!")
+
+});
+
+.on("click", "#green-crystal", function() {
+
+ 
+alert("You clicked a crystal!")
+
+});
+
+.on("click", "#blue-crystal", function() {
+
+ 
+alert("You clicked a crystal!")
+
+});
+
+.on("click", "#white-crystal", function() {
+
+ 
+alert("You clicked a crystal!")
+
+});
+
+
+
+</script>
+
+
